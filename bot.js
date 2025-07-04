392d7ae05a7fe26063cf7a674130db9853136dfc
Your Name <bahrom9791@gmail.com git config --global user.name>
2025-07-04 11:41:17 +0500

    fix: добавлены обратные кавычки в setWebHook

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ const url = process.env.WEBHOOK_URL;
 const port = process.env.PORT;
 
 const bot = new TelegramBot(token);
-bot.setWebHook(${url}/bot${token});
+bot.setWebHook(`${url}/bot${token}`);
 
 const app = express();
 app.use(express.json());
